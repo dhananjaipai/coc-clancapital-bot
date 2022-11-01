1d496bc566f1e135fbd58bd28a7890f48b4045c5
Dhananjai Pai <dhananjaimvpai@gmail.com>
2022-11-01 17:59:32 +0530

    add api endpoints for sheets

--- utils/telegram.mjs
+++ utils/telegram.mjs
@@ -9,6 +9,13 @@ const bot_token = process.env.TELEGRAM_TOKEN;
 const chat_id = process.env.CHAT_ID;
 const PORT = 3000;
 
+const fetch = URI => _fetch(URI, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${process.env.CLASH_API_TOKEN}`,
+    }
+});
+
 const url = await ngrok.connect({ authtoken: ngrok_token, addr: PORT });
 export const bot = new Telegraf(bot_token);
 const secretPath = `/telegraf/${bot.secretPathComponent()}`;
@@ -17,6 +24,30 @@ bot.telegram.setWebhook(`${url}${secretPath}`);
 const app = express();
 app.get("/", (_, res) => res.send("OK"));
 app.use(bot.webhookCallback(secretPath));
+
+app.get("/playerClan/:tag", async (req, res) => {
+    const {tag}=req.params;
+    const data = await fetch(
+        `https://api.clashofclans.com/v1/players/${encodeURIComponent(tag)}`
+      ).then(response => response.json());
+    res.send(data?.clan?.name);
+});
+app.get("/playerName/:tag", async (req, res) => {
+    const {tag}=req.params;
+    const data = await fetch(
+        `https://api.clashofclans.com/v1/players/${encodeURIComponent(tag)}`
+      ).then(response => response.json());
+    res.send(data?.name);
+});
+app.get("/clanMembers/:tag", async (req, res) => {
+    const {tag}=req.params;
+    const data = await fetch(
+        `https://api.clashofclans.com/v1/clans/${encodeURIComponent(tag)}/members`
+      ).then(response => response.json());
+    res.send(data?.items.map(({name}) => name));
+});
+
+
 app.listen(PORT, () => {
   console.log(`Listening on local port ${PORT}`);
   console.log(`Ngrok running at ${url}`);
