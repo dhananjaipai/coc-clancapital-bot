a59778276038846d361d27b9acc267b5ca8d5eb8
dhananjaipai <dhananjaimvpai@gmail.com>
2022-12-23 23:32:21 +0000

    revert to sqlite

--- db/index.mjs
+++ db/index.mjs
@@ -3,26 +3,19 @@ import _Member from "./models/Member.mjs";
 import _Record from "./models/Record.mjs";
 import * as fs from "fs";
 
-export const DATABASE_USERNAME = process.env.DATABASE_USERNAME;
-export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
-export const DATABASE_HOST = process.env.DATABASE_HOST;
-export const DATABASE_NAME = process.env.DATABASE_NAME;
-
-export const sequelize = new Sequelize(
-  DATABASE_NAME,
-  DATABASE_USERNAME,
-  DATABASE_PASSWORD,
-  {
-    dialect: "mysql",
-    host: DATABASE_HOST,
-    logging: false,
-  }
-);
+export const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./members.sqlite",
+  logging: false,
+  retry: {
+    max: 20,
+  },
+});
 
 export const Member = sequelize.define("Member", _Member);
 export const Record = sequelize.define("Record", _Record);
 export const QUERY_SUMMARY = fs.readFileSync(
-  new URL("./query/summary.sql", import.meta.url),
+  new URL('./query/summary.sql', import.meta.url),
   { encoding: "utf8", flag: "r" }
 );
 try {
